e549e7710d52dc4949559f1f2bc076877a822f9c
OnGod123 <123314086+OnGod123@users.noreply.github.com>
2023-07-30 06:50:59 +0100

    Add files via upload
    Comprehensive task on arduino IDE.

---
+++ header_library_in_embedded.c
@@ -0,0 +1,12 @@
+/**
+*including header file for all microcontroller
+* #include <usbhost.h>  this is for arduino native usb port.
+* #include <Arduino.h> this for arduino (port programming)
+* #include <Arduino.h> this is for arduino (Diecimilia)
+* #include <Adafruit_CircuitPlayground.h>  this header file is for(Adafruit_circuit_playground)
+* #include <esp_log.h>  the two following header file is for ALKS_ESP32
+* #include <esp_wifi.h>
+* #include <driver/gpio.h>
+* //
+* 
+**/
